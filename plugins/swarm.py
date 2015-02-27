028b31772d43817cc7bb72ddd55a4bc6f2e56678
Olivier Sallou <olivier.sallou@irisa.fr>
2015-02-27 17:51:20 +0100

    fix indent

--- plugins/swarm.py
+++ plugins/swarm.py
@@ -1,4 +1,5 @@
 from godocker.iExecutorPlugin import IExecutorPlugin
+import json
 
 
 class Swarm(IExecutorPlugin):
@@ -22,18 +23,23 @@ class Swarm(IExecutorPlugin):
         :return: tuple of submitted and rejected/errored tasks
         '''
         running_tasks = []
+        error_tasks = []
         for task in tasks:
-            job  = json.loads(task)
-            container = self.docker_client.create_container(image=job['container']['image'],
-                                                            command=job['command']['cmd'],
-                                                            cpu_shares=job['requirements']['cpu'],
-                                                            mem_limit=str(job['requirements']['ram'])+'g',
-                                                            ports=[22])
-            response = cli.start(container=container.get('Id'),
-                                    network_mode='host',
-                                    port_bindings={22: None})
-            job['container']['id'] = container['Id']
-            running_tasks.append(json.dumps(job))
+            try:
+                job  = json.loads(task)
+                container = self.docker_client.create_container(image=job['container']['image'],
+                                                                command=job['command']['cmd'],
+                                                                cpu_shares=job['requirements']['cpu'],
+                                                                mem_limit=str(job['requirements']['ram'])+'g',
+                                                                ports=[22])
+                response = self.docker_client.start(container=container.get('Id'),
+                                        network_mode='host',
+                                        port_bindings={22: None})
+                job['container']['id'] = container['Id']
+                running_tasks.append(json.dumps(job))
+            except Exception as e:
+                logging.error(e.strerror)
+                error_tasks.append(task)
         return (running_tasks,[])
 
 
