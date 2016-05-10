17c3043ec001386e510768b21df064e22c4c7552
Olivier Sallou <olivier.sallou@irisa.fr>
2016-05-10 16:54:35 +0200

    udpate help message

--- go-d-scheduler.py
+++ go-d-scheduler.py
@@ -41,5 +41,5 @@ if __name__ == "__main__":
                         sys.exit(2)
                 sys.exit(0)
         else:
-                print("usage: %s start|stop|restart|init|once" % sys.argv[0])
+                print("usage: %s start|stop|restart|init|once|config-reload" % sys.argv[0])
                 sys.exit(2)
