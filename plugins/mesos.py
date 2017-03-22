53dcf413a1565056de6a19ebb6168f8f9db2382e
kys10 <skyisno.1@gmail.com>
2017-03-22 22:45:54 +0800

    currently container not supported

--- plugins/mesos.py
+++ plugins/mesos.py
@@ -10,6 +10,7 @@ import urllib3
 import socket
 import signal
 import getpass
+import uuid
 
 from threading import Thread
 from bson.json_util import dumps
@@ -37,8 +38,8 @@ class MesosThread(Thread):
     def run(self):
         self.driver.run()
 
-    def stop(self):
-        self.driver.stop()
+    def stop(self, failover):
+        self.driver.stop(failover=failover)
 
 
 class MesosScheduler(Scheduler):
@@ -64,19 +65,48 @@ class MesosScheduler(Scheduler):
     def set_logger(self, logger):
         self.logger = logger
 
+    def registered(self, driver, frameworkId, masterInfo):
+        self.logger.info("Registered with framework ID %s" % frameworkId.value)
+        self.redis_handler.set(
+            self.config['redis_prefix'] + ':mesos:frameworkId',
+            frameworkId.value
+            )
+
+        # Reconcile at startup
+        # if 'mesos' in self.config and 'reconcile' in self.config['mesos'] and self.config['mesos']['reconcile']:
+        #     self.logger.info("Reconcile any running task")
+        #     tasks = self.jobs_handler.find({'status.primary': 'running'})
+        #     running_tasks = []
+        #     for task in tasks:
+        #         task_status = mesos_pb2.TaskStatus()
+        #         task_id = mesos_pb2.TaskID()
+        #         task_id.value = str(task['id'])
+        #         task_status.task_id.MergeFrom(task_id)
+        #         task_status.state = 1
+        #         self.logger.debug('Mesos:Reconcile:Task:' + str(task['id']))
+        #         running_tasks.append(task_status)
+        #     driver.reconcileTasks(running_tasks)
+
+
     def resourceOffers(self, driver, offers):
         filters = {'refuse_seconds': 5}
         for offer in offers:
-            self.logger.debug('Recieving offer:' + json.dumps(offer))
+
+            self.logger.debug('Recieving offer:' + offer.hostname)
 
             available_tasks = self.getAvailableTasks(offer)
 
             tasks_to_launch = self.addOfferInfo(available_tasks, offer)
 
+            # TODO: for test only. mock task
+            tasks_to_launch[0].task_id.value = str(uuid.uuid4())
+            tasks_to_launch[0].container.mesos.image.docker.name='busybox'
+            self.logger.debug('launching task:' + str(tasks_to_launch))
             driver.launchTasks(offer.id, tasks_to_launch, filters)
 
     def getResource(self, res, name):
         for r in res:
+            r = Dict(r)
             if r.name == name:
                 return r.scalar.value
         return 0
@@ -91,44 +121,79 @@ class MesosScheduler(Scheduler):
         Get the list of tasks from database or redis cache
         '''
         tasks = []
-        redis_task = self.redis_handler.lpop(self.config['redis_prefix'] + ':mesos:pending')
-        while redis_task is not None:
+        redis_tasks = self.redis_handler.lrange(self.config['redis_prefix'] + ':mesos:pending', 0, -1)
+        for redis_task in redis_tasks:
             task = json.loads(redis_task)
             task = self.taskAdapter(task)
-            self.logger.debug('loading tasks from database: ' + json.dumps(task))
+            # self.logger.debug('loading tasks from database: ' + json.dumps(task))
             tasks.append(task)
-            redis_task = self.redis_handler.lpop(self.config['redis_prefix'] + ':mesos:pending')
-
+            
         return tasks
 
-    def taskAdapter(self, redis_task):
+    def taskAdapter(self, request_task):
         '''
         transform the json formatted redis task into a mesos task, without the execution info
         '''
         task = Dict()
-        redis_task = Dict(redis_task)
-        task.task_id = redis_task.id
-        task.name = redis_task.meta.name
-        requirements = redis_task.requirements
+        request_task = Dict(request_task)
+        task.task_id.value = str(request_task.id)
+        task.name = request_task.meta.name
+        requirements = request_task.requirements
         task.resources = [
             dict(name='cpus', type='SCALAR', scalar={'value': requirements.cpu}),
             dict(name='mem', type='SCALAR', scalar={'value': requirements.ram * 1000}),
             dict(name='gpus', type='SCALAR', scalar={'value': requirements.gpu or 0}),
         ]
 
-        # containerizer
-        container = Dict(type='MESOS')
-        # image provider
-        container.mesos = Dict(type='DOCKER', docker={'name':redis_task.container.image})
-        task.container = container
 
-        task.executor.command = Dict(value=redis_task.command.script)
+        task.container = self.containerAdapter(request_task.container)
+
+
+        task.executor.command = Dict(value=request_task.command.script)
+        task.executor.executor_id.value = 'MinimalExecutor'
 
         return task
 
+    def containerAdapter(self, request_container):
+        # containerizer
+        container = Dict(type='MESOS')
+        # image provider
+        container.mesos.image = Dict(
+            type='DOCKER',
+            docker={'name':request_container.image}
+            )
+
+        #for debug
+        container.type = 'DOCKER'
+        container.docker.image = 'busybox'
+        container.volumes = self.volumesAdapter(request_container.volumes)
+        return container
+
+    def volumesAdapter(self, request_volumes):
+        volumes = []
+        for request_volume in request_volumes:
+            volume = self.volumeAdapter(request_volume)
+            volumes.append(volume)
+        return volumes
+
+    def volumeAdapter(self, request_volume):
+        volume = Dict()
+
+        # the container path is default to host path if not set
+        volume.host_path = request_volume.path
+        volume.container_path = request_volume.mount or volume.path
+
+        if request_volume.acl == 'rw':
+            volume.mode = 'RW'
+        else:
+            volume.mode = 'RO'
+        return volume
+
     def getAvailableTasks(self, offer):
+        self.pending_tasks = self.getTasksFromDB()
         for pending_task in self.pending_tasks:
             if self.hasEnoughResource(offer,pending_task):
+                self.logger.debug('\nget pending tasks: '.join(pending_task.task_id))
                 return [pending_task]
         return []
 
@@ -143,7 +208,7 @@ class MesosScheduler(Scheduler):
 
     def getResourcesNames(self, resources):
 
-        return set([resource.name for resource in resources])
+        return set([resource['name'] for resource in resources])
 
 
     def hasEnoughResource(self, offer, task):
@@ -154,7 +219,7 @@ class MesosScheduler(Scheduler):
         requested_resources = task.resources
         for resource_name in self.getResourcesNames(requested_resources):
             if self.getResource(offer_resources, resource_name) < self.getResource(requested_resources, resource_name):
-                logging.warn("Not enough %s" % resource_name)
+                self.logger.warn("Not enough %s" % resource_name)
                 return False
         return True
 
@@ -210,19 +275,24 @@ class Mesos(IExecutorPlugin):
         if prevFrameworkId:
             # Reuse previous framework identifier
             self.logger.info("Mesos:FrameworkId:" + str(prevFrameworkId))
-            framework.id = prevFrameworkId
+            framework.id.value = prevFrameworkId
         if os.getenv("MESOS_CHECKPOINT"):
             self.logger.info("Enabling checkpoint for the framework")
             framework.checkpoint = True
 
+        mesos_scheduler = MesosScheduler(executor)
+        mesos_scheduler.set_logger(self.logger)
+        mesos_scheduler.set_config(self.cfg)
         driver = MesosSchedulerDriver(
-            MesosScheduler(executor),
+            mesos_scheduler,
             framework,
             self.cfg['mesos']['master'],
             use_addict=True,
         )
 
-        driver_thread = Thread(MesosThread(driver))
+        driver_thread = MesosThread()
+        driver_thread.set_driver(driver)
+        self.driver = driver_thread
         driver_thread.start()
 
         print('Scheduler running, Ctrl+C to quit.')
@@ -233,7 +303,7 @@ class Mesos(IExecutorPlugin):
         Request end of executor if needed
         '''
         if self.driver is not None:
-            self.driver.stop(True)
+            self.driver.stop(failover=True)
         self.Terminated = True
 
     def run_all_tasks(self, tasks, callback=None):
